463a65565272b2036b1fb67d059d5ec90787e1bb
Odd13 <arcadeoddie@gmail.com>
2018-01-26 22:55:54 +1100

    Add RssFeed class and removed old references.
    Added this to the main.py file.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import time
 from DeskLight import DeskLight
+from RssFeed import RssFeed
 
 desklight1 = DeskLight()
+feed1 = RssFeed("https://www.ozbargain.com.au/feed")
 
 while True:
-  desklight1.flash(5,3)
-  time.sleep(2)
-  desklight1.dim_up_and_down()
+	if (feed1.has_it_changed() == True):
+		desklight1.flash(2,3)
+		print(feed1.current_title)
+		print(feed1.previous_title)	
+		desklight1.dim_up_and_down()
+		desklight1.turn_off()
+	else:
+		print("No Change")
+		time.sleep(300)
